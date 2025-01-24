00bcc59ba6c919f62adb53b90491e8cc27e98c9c
Shantanu singh <shantanu.kr025@gmail.com>
2025-01-24 13:29:41 +0530

    flirtbate

--- src/components/EffectCardsScroller.js
+++ src/components/EffectCardsScroller.js
@@ -3,6 +3,8 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, EffectCards } from 'swiper/modules';
 
+import styles from '../styles/EffectCardsScroller.module.css';
+
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 
@@ -14,7 +16,7 @@ const EffectCardsScroller = () => {
   ];
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
+    <div className={styles.container}>
       <Swiper
         modules={[Autoplay, EffectCards]}
         effect="cards"
@@ -25,11 +27,11 @@ const EffectCardsScroller = () => {
           delay: 3000,
           disableOnInteraction: false,
         }}
-        style={{ width: '70%', position: 'relative',height:"90%" }} // Adjust width as needed
+        className={styles.swiperContainer}
       >
         {images.map((image, i) => (
-          <SwiperSlide key={i} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center', borderRadius: '8px', overflow: 'hidden' }}>
-            <img src={image} alt={`Slide ${i + 1}`} style={{borderRadius:'60px', maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} />
+          <SwiperSlide key={i} className={styles.swiperSlide}>
+            <img src={image} alt={`Slide ${i + 1}`} className={styles.slideImage} />
           </SwiperSlide>
         ))}
       </Swiper>
